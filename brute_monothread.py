354f44411f095b3951a9049cb78a759dd25d4aa8
JonatanaRandria <hei.jonatana@gmail.com>
2024-03-17 17:23:58 +0300

    edit: Readme.md

--- brute_monothread.py
+++ brute_monothread.py
@@ -13,16 +13,10 @@ with open("dir_list.txt", "r") as fichier:
     for i in fichier:
         
         url = "http://127.0.0.1:5000/" + i.strip() 
-        print('Testing : '+ url)
+        print('Testing : '+ i)
         response = requests.get(url)
         if response.status_code != 404:
             checked.append(url)
-    print('Working link :')
+    print('Found the following path :')
     for g in checked:
         print(g)
-fin = time.time()
-
-# Calculer la durée d'exécution
-duree = fin - debut
-print(duree)
-
