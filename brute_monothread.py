e382cb276cbaf89f83b240c4cf1824785e66692f
JonatanaRandria <hei.jonatana@gmail.com>
2024-03-17 17:33:44 +0300

    style:comment monothread test

--- brute_monothread.py
+++ brute_monothread.py
@@ -1,22 +1,28 @@
 import requests
-import time
 
-checked=[]
+# Initialize an empty list to store checked URLs
+checked = []
 
-
-
+# Open the file containing directory paths for checking
 with open("dir_list.txt", "r") as fichier:
-    time.sleep(2)  # Exemple de code, à remplacer par votre propre code
-
-# Enregistrer le temps de début
-    debut = time.time()
+    
+    # Iterate over each line in the file
     for i in fichier:
         
+        # Construct the URL by stripping whitespace and appending to the base URL
         url = "http://127.0.0.1:5000/" + i.strip() 
+        
+        # Print the current path being tested
         print('Testing : '+ i)
+        
+        # Send a GET request to the URL
         response = requests.get(url)
+        
+        # If the response status code is not 404 (Not Found), add the URL to the checked list
         if response.status_code != 404:
             checked.append(url)
-    print('Found the following path :')
-    for g in checked:
-        print(g)
+
+# Print the list of found paths
+print('Found the following path :')
+for g in checked:
+    print(g)
